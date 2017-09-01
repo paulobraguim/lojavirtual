00e96bc5e899aecae98bec95214ecc069ffa2154
paulobraguim <paulo.braguim@outlook.com>
2017-09-01 14:05:54 -0300

    Admin login

--- index.php
+++ index.php
@@ -1,10 +1,13 @@
 <?php 
 
+session_start();
+
 require_once("vendor/autoload.php");
 
 use \Slim\Slim;
 use \Hcode\Page;
 use \Hcode\PageAdmin;
+use \Hcode\Model\User;
 
 $app = new Slim();
 
@@ -20,12 +23,46 @@ $app->get('/', function() {
 
 // Rota admin
 $app->get('/admin', function() {
+
+	User::verifyLogin();
     
 	$page = new PageAdmin();
 
 	$page->setTpl("index");
 
 });
+
+// Rota login admin
+$app->get('/admin/login', function() {
+    
+	$page = new PageAdmin([
+		"header"=>false,
+		"footer"=>false
+	]);
+
+	$page->setTpl("login");
+});
+
+// Rota para validar login
+$app->post('/admin/login', function(){
+
+	User::login($_POST["login"], $_POST["password"]);
+
+	header("Location: /admin");
+	exit;
+});
+
+// Rota para logout
+$app->get('/admin/logout', function() {
+    
+	User::logout();
+
+	header("Location: /admin/login");
+
+	exit;
+
+});
+
 
 $app->run();
 
