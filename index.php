e486b6eb090c61b9a96fa34a8dee795afca0dab4
paulobraguim <paulo.braguim@outlook.com>
2017-09-03 12:48:44 -0300

    Admin CRUD

--- index.php
+++ index.php
@@ -63,6 +63,103 @@ $app->get('/admin/logout', function() {
 
 });
 
+// Rota para listar os users
+$app->get('/admin/users', function(){
+
+	User::verifyLogin();
+
+	$users = User::listAll();
+
+	$page = new PageAdmin();
+
+	$page->setTpl("users", array(
+		"users"=>$users
+	));
+
+});
+
+// Rota para cadastrar os users
+$app->get('/admin/users/create', function(){
+
+	User::verifyLogin();
+
+	$page = new PageAdmin();
+
+	$page->setTpl("users-create");
+
+});
+
+// Rota para deletar um usuário
+$app->get('/admin/users/:iduser/delete', function($iduser){
+
+	User::verifyLogin();
+
+	$user = new User();
+
+	$user->get((int)$iduser);
+
+	$user->delete();
+
+	header("Location: /admin/users");
+	exit;
+	
+});
+
+// Rota para alterar os users
+$app->get('/admin/users/:iduser', function($iduser){
+
+	User::verifyLogin();
+
+	$user = new User();
+
+	$user->get((int)$iduser);	
+
+	$page = new PageAdmin();
+
+	$page->setTpl("users-update", array(
+		"user"=>$user->getValues()
+	));
+
+});
+
+// Salvar create dos users
+$app->post('/admin/users/create', function(){
+
+	User::verifyLogin();
+
+	$user = new User();
+
+	$_POST["inadmin"] = (isset($_POST["inadmin"]))? 1:0;	 
+
+	$user->setData($_POST);
+
+	$user->save();
+
+	header("Location: /admin/users");
+	exit;
+
+});
+
+// Salvar o update do users
+$app->post('/admin/users/:iduser', function($iduser){
+
+	User::verifyLogin();
+
+	$user = new User();
+
+	$_POST["inadmin"] = (isset($_POST["inadmin"]))? 1:0;
+
+	$user->get((int)$iduser);
+
+	$user->setData($_POST);
+
+	$user->update();
+
+	header("Location: /admin/users");
+	exit;
+	
+});
+
 
 $app->run();
 
