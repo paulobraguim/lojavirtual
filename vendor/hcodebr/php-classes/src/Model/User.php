98e8b207094118820cec3a3334b78cfef8524250
paulobraguim <paulo.braguim@outlook.com>
2017-09-18 15:24:33 -0300

    Esqueci a senha site

--- vendor/hcodebr/php-classes/src/Model/User.php
+++ vendor/hcodebr/php-classes/src/Model/User.php
@@ -196,10 +196,13 @@ class User extends Model {
 			{
 				$dataRecovery = $results2[0];
 				$code = base64_encode(mcrypt_encrypt(MCRYPT_RIJNDAEL_128, User::SECRET, $dataRecovery["idrecovery"], MCRYPT_MODE_ECB));
+
 				if ($inadmin === true) {
 					
 					$link = "http://www.hcodecommerce.com.br/admin/forgot/reset?code=$code";
+
 				} else {
+					
 					$link = "http://www.hcodecommerce.com.br/forgot/reset?code=$code";
 				}
 
