1b98278862feeff089d20cc33c1a66dfbd52a41c
paulobraguim <paulo.braguim@outlook.com>
2017-09-21 21:17:43 -0300

    WEB Service Endereço

--- vendor/hcodebr/php-classes/src/Model/Address.php
+++ vendor/hcodebr/php-classes/src/Model/Address.php
@@ -38,10 +38,48 @@ class Address extends Model {
 			$this->setdescity($data['localidade']);
 			$this->setdesstate($data['uf']);
 			$this->setdescountry('Brasil');
-			$this->setnrzipcode($nrcep);
+			$this->setdeszipcode($nrcep);
 		}
 	}
+
+	public function save()
+	{
+		$sql = new Sql();
+
+		$results = $sql->select("CALL sp_addresses_save(:idaddress, :idperson, :desaddress, :descomplement, :descity, :desstate, :descountry, :deszipcode, :desdistrict)", [
+			':idaddress'=>$this->getidaddress(),
+			':idperson'=>$this->getidperson(),
+			':desaddress'=>utf8_decode($this->getdesaddress()),
+			':descomplement'=>utf8_decode($this->getdescomplement()),
+			':descity'=>utf8_decode($this->getdescity()),
+			':desstate'=>utf8_decode($this->getdesstate()),
+			':descountry'=>utf8_decode($this->getdescountry(),
+			':deszipcode'=>$this->getdeszipcode(),
+			':desdistrict'=>$this->getdesdistrict()
+		]);
+
+		if (count($results) > 0) {
+			
+			$this->setData($results[0]);
+		}
+	}	
+
+	public static function setMsgError($msg)
+	{
+		$_SESSION[Address::SESSION_ERROR] = $msg;
+	}
+
+	public static function getMsgError()
+	{
+		$msg = (isset($_SESSION[Address::SESSION_ERROR])) ? $_SESSION[Address::SESSION_ERROR] : "";
+		Address::clearMsgError();
+		return $msg;
+	}
 	
+	public static function clearMsgError()
+	{
+		$_SESSION[Address::SESSION_ERROR] = NULL;
+	}
 }	
 
- ?>
+?>
