49e10e45df030e29ffce0f6d0c9f5d14fa4f0d68
paulobraguim <paulo.braguim@outlook.com>
2017-10-04 15:28:25 -0300

    Admin - Categorias - Busca e Paginação

--- admin-categories.php
+++ admin-categories.php
@@ -10,12 +10,38 @@ $app->get("/admin/categories", function(){
 
 	User::verifyLogin();
 
-	$categories = Category::listAll();
+	$search = (isset($_GET['search'])) ? $_GET['search'] : "";
+
+	$page = (isset($_GET['page'])) ? (int)$_GET['page'] : 1;
+
+	if ($search != '') {
+		
+		$pagination = Category::getPageSearch($search, $page);
+
+	} else {
+
+		$pagination = Category::getPage($page);
+	}
+
+	$pages = [];
+
+	for ($i=0; $i < $pagination['pages'] ; $i++) { 
+		
+		array_push($pages, [
+			'href'=>'/admin/categories?' . http_build_query([
+				'page'=>$i + 1,
+				'search'=>$search
+			]),
+			'text'=>$i + 1	
+		]);
+	}
 
 	$page = new PageAdmin();
 
-	$page->setTpl("categories", [
-		"categories"=>$categories
+	$page->setTpl("categories", [		
+		"categories"=>$pagination['data'],
+		"search"=>$search,
+		"pages"=>$pages		
 	]);
 
 });
